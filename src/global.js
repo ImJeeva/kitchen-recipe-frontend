03ea655e552f521c001a51c64519ca6b7051596f
ImJeeva <romanjeeva22@gmail.com>
2026-05-15 17:07:59 +0530

    use direct backend URL without proxy

--- src/global.js
+++ src/global.js
@@ -1,2 +1,1 @@
-const BASE_URL = "https://kitchen-recipe-backend-lo63nhrpg-jeeva-s-projects1.vercel.app";
-export const API = `${BASE_URL}/api`;
+export const API = "https://kitchen-recipe-backend-lo63nhrpg-jeeva-s-projects1.vercel.app";
